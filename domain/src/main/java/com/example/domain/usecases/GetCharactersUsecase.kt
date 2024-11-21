6bd3beb10b4548f4ef09cb942d094c4e0f4d35a8
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2024-11-21 14:36:38 +0530

    Domain and Data Layer for Homescreen: PR changes

--- domain/src/main/java/com/example/domain/usecases/GetCharactersUsecase.kt
+++ domain/src/main/java/com/example/domain/usecases/GetCharactersUsecase.kt
@@ -3,12 +3,11 @@ package com.example.domain.usecases
 
 import com.example.domain.models.CharacterSchema
 import com.example.domain.repository.CharacterRepository
-import com.example.domain.utils.Results
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetCharactersUsecase @Inject constructor(private val repository: CharacterRepository){
-    suspend  fun invoke(): Flow<Results<List<CharacterSchema>>> {
+    suspend  fun invoke(): Flow<com.example.domain.utils.Results<List<CharacterSchema?>>> {
         return repository.getCharacters()
     }
 }
