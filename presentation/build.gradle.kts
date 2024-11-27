4e5922a88468f56974cc937c54e5f2508926d813
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2024-11-27 12:00:52 +0530

    Presentation Layer for rebase Homescreen

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -30,14 +32,31 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-
+    implementation(project(":domain"))
+    implementation(project(":common"))
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.dagger)
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.androidx.material3.android)
+    ksp(libs.dagger.compiler)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.coil.compose)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
