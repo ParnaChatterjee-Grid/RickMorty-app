b50e7904c36e3a86cc69908431282d96e3b22ec3
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2024-11-29 16:32:11 +0530

    CustomException added, detekt.yml changed , solved exceptions :

--- common/src/main/java/com/example/common/CustomExceptions.kt
+++ common/src/main/java/com/example/common/CustomExceptions.kt
@@ -3,12 +3,12 @@ package com.example.common
 sealed class CustomExceptions(message: String) : Exception(message) {
 
  // Represents network-related errors
- class ApolloNetworkError(cause: Throwable? = null) : Exception(cause)
+ class ApolloNetworkError(cause: Throwable? = null) : CustomExceptions(cause?.message ?: "Apollo Network error")
  // Represents Socket errors
- class ApolloWebSocketClosedError(cause: Throwable? = null) : Exception(cause)
+ class ApolloWebSocketClose(cause: Throwable? = null) : CustomExceptions(cause?.message ?: "Apollo WebSocket error")
  // Represents unexpected errors
- class ApolloGraphQLError(cause: Throwable? = null) : Exception(cause)
+ class ApolloGraphQLError(cause: Throwable? = null) : CustomExceptions(cause?.message ?: "ApolloGraphQLError")
  // Represents invalid input errors
- class ApolloHttpError(cause: Throwable? = null) : Exception(cause)
+ class ApolloHttpError(cause: Throwable? = null) : CustomExceptions(cause?.message ?: "ApolloHttpError")
 }
 
