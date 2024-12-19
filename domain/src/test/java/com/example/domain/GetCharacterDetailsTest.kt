c5b575219d676bc8e0692e3792a21733df5fafdf
ParnaChatterjee-Grid <pchatterjee@griddynamics.com>
2024-12-19 12:51:06 +0530

    Testcases change as per PR comment.

--- domain/src/test/java/com/example/domain/GetCharacterDetailsTest.kt
+++ domain/src/test/java/com/example/domain/GetCharacterDetailsTest.kt
@@ -17,54 +17,68 @@ import org.junit.Test
 
 @kotlinx.coroutines.ExperimentalCoroutinesApi
 class GetCharacterDetailsTest {
-    private lateinit var getCharacterDetails : GetCharacterDetailsUsecase
+    private lateinit var getCharacterDetails: GetCharacterDetailsUsecase
     private val repository = mockk<CharacterRepository>(relaxed = true)
     private val testDispatcher = StandardTestDispatcher()
+
     @Before
-    fun setup(){
+    fun setup() {
         getCharacterDetails = GetCharacterDetailsUsecase(repository)
     }
 
     @Test
-    fun `should return details of character when repository returns data`() = runTest(testDispatcher) {
-        val expectedResult = CharacterDetails(
-            id = "1",
-            name = "Rick",
-            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-            status = "Alive",
-            species = "Human",
-            gender = "male",
-            origin = Origins(name = "Earth", dimension = "Replacement Dimension"),
-            locations = Locations(id = "1", name = "Earth", dimension = "Dimension"),
-            episodes = listOf(
-                Episode("6", "Rick Potion #9", "January 27, 2014","Episode1"),
-                Episode("7", "Raising Gazorpazorp", "March 10, 2014","Episode2")
+    fun `when CharacterRepository returns data then get character details on success`() =
+        runTest(testDispatcher) {
+            //Given
+            val expectedResult = CharacterDetails(
+                id = "1",
+                name = "Rick",
+                image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+                status = "Alive",
+                species = "Human",
+                gender = "male",
+                origin = Origins(name = "Earth", dimension = "Replacement Dimension"),
+                locations = Locations(id = "1", name = "Earth", dimension = "Dimension"),
+                episodes = listOf(
+                    Episode("6", "Rick Potion #9", "January 27, 2014", "Episode1"),
+                    Episode("7", "Raising Gazorpazorp", "March 10, 2014", "Episode2")
+                )
             )
-        )
 
-        coEvery { repository.getCharacterById("1") } returns expectedResult
-        val testCaseCharacters = getCharacterDetails.invoke("1")
-        assertEquals(expectedResult,testCaseCharacters)
-    }
+            coEvery { repository.getCharacterById("1") } returns expectedResult
+
+            //When
+            val testCaseCharacters = getCharacterDetails.invoke("1")
+            //Then
+            assertEquals(expectedResult, testCaseCharacters)
+        }
 
     @Test
-    fun ` for runtime exception to show error`() = runTest(testDispatcher) {
-        val exception = RuntimeException("Error fetching character results")
-        coEvery { repository.getCharacterById("1") } throws exception
-        try {
+    fun `when CharacterRepository got exception then GetCharacterDetailsUsecase caught the error`() =
+        runTest(testDispatcher) {
+            //Given
+            val exception = RuntimeException("Error fetching character results")
+            coEvery { repository.getCharacterById("1") } throws exception
+            //When
+            try {
+                val result = getCharacterDetails.invoke("1")
+            }
+            //Then
+            catch (e: RuntimeException) {
+                assertEquals("java.lang.RuntimeException: Error fetching character results", "" + e)
+            }
+        }
+
+    @Test
+    fun `when CharacterRepository returns no data then returns empty data`() =
+        runTest {
+            //Given
+            val emptyCharacterDetails = null
+            // Mocking the suspending function using coEvery
+            coEvery { repository.getCharacterById("1") } returns emptyCharacterDetails
+            //When
             val result = getCharacterDetails.invoke("1")
+            //Then
+            assertEquals(result, emptyCharacterDetails)
         }
-        catch( e :RuntimeException)  {
-            assertEquals("java.lang.RuntimeException: Error fetching character results", ""+e)
-        }
-    }
-
-    @Test
-    fun `should return empty character details when repository returns no data`() = runTest {
-         val emptyCharacterDetails = null
-        // Mocking the suspending function using coEvery
-        coEvery { repository.getCharacterById("1") } returns emptyCharacterDetails
-        val result = getCharacterDetails.invoke("1")
-        assertEquals(result,emptyCharacterDetails)
-    }
 }
